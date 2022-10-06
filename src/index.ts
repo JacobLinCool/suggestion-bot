4e5f7073fde19a7b14a2e5d1e41fb3ba2537bc0f
JacobLinCool <jacoblincool@gmail.com>
2022-10-06 23:44:52 +0800

    feat: use custom control prefix

--- src/index.ts
+++ src/index.ts
@@ -7,14 +7,12 @@ import { Suggestion } from "pure-cat-module-suggestion";
 new Bot()
     .use(new Ignore())
     .use(new MongoStore(process.env.MONGO_URL || ""))
-    .use(new I18n({ fallback: "en", table: {} }))
+    .use(new I18n({ controller_prefix: "!linter lang" }))
     .use(
         new Suggestion({
+            controller_prefix: "!linter",
             pass(content: string) {
-                if (content.length > 100 || content.includes("no-check")) {
-                    return true;
-                }
-                return false;
+                return content.length > 100 || content.includes("no-check");
             },
         }),
     )
@@ -22,5 +20,5 @@ new Bot()
     .then(() => console.log("Bot started!"));
 
 /**
- * Invite link: https://discord.com/oauth2/authorize?client_id=1026910413067137054&permissions=274877910016&scope=bot
+ * Invitation: https://discord.com/oauth2/authorize?client_id=1026910413067137054&permissions=274877910016&scope=bot
  */
